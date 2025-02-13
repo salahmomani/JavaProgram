42df13b3ea5bb3f1792d3a4f2692d49211d17a3a
salahmomani <slhalmomani@gmail.com>
2025-02-13 14:45:03 +0300

    commit

---
+++ src/part2/Methods/word.java
@@ -0,0 +1,15 @@
+package part2.Methods;
+
+import java.util.Scanner;
+
+public class word {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(word());
+    }
+    public static String word(){
+        return" Atypon";
+
+
+    }
+}
