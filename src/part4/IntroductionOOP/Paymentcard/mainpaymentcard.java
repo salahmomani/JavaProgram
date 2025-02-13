8581f7cd4814891d5330021ed2d5f8ed767148ce
salahmomani <slhalmomani@gmail.com>
2025-02-14 02:57:04 +0300

    committ

--- src/part4/IntroductionOOP/Paymentcard/mainpaymentcard.java
+++ src/part4/IntroductionOOP/Paymentcard/mainpaymentcard.java
@@ -3,6 +3,7 @@ package part4.IntroductionOOP.Paymentcard;
 public class mainpaymentcard {
     public static void main(String[] args) {
         Paymentcard card = new Paymentcard(10);
+        /// ////////////part1+2+3///////////////////
 //        System.out.println(card);
 //        card.eatHeartily();
 //
@@ -12,16 +13,31 @@ public class mainpaymentcard {
 //        card.eatHeartily();
 //       // card.eatAffordably();
 //        System.out.println(card);
-
-        System.out.println(card);
-
-        card.addMoney(15);
-        System.out.println(card);
-
-        card.addMoney(10);
-        System.out.println(card);
-
-        card.addMoney(-200);
-        System.out.println(card);
+/// //////////part4+part5//////////
+//        System.out.println(card);
+//
+//        card.addMoney(15);
+//        System.out.println(card);
+//
+//        card.addMoney(10);
+//        System.out.println(card);
+//
+//        card.addMoney(-200);
+//        System.out.println(card);
+        Paymentcard paulsCard = new Paymentcard(20);
+        Paymentcard mattsCard = new Paymentcard(30);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("paul "+paulsCard.toString());
+        System.out.println("matt "+mattsCard.toString());
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("paul "+paulsCard.toString());
+        System.out.println("matt "+mattsCard.toString());
+paulsCard.eatAffordably();
+paulsCard.eatAffordably();
+mattsCard.addMoney(50);
+        System.out.println("paul "+paulsCard.toString());
+        System.out.println("matt "+mattsCard.toString());
     }
 }
