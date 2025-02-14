e9aba8c65b2f46ad327cabfda397f33e68255497
salahmomani <slhalmomani@gmail.com>
2025-02-14 23:51:52 +0300

    committ

--- src/part4/fileandreadingdata/Cubes.java
+++ src/part4/fileandreadingdata/Cubes.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Cubes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String number;
+        int result;
+        while (true)
+        {
+            number=in.next();
+            if(number.equals("end")){
+                break;
+            }
+             result = Integer.parseInt(number);
+            System.out.println(result*result*result);
+        }
 
     }
 }
