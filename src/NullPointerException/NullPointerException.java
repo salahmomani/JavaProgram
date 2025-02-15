3129894987c12ccc2cccd067e806b8c9a6140ab7
salahmomani <slhalmomani@gmail.com>
2025-02-16 01:36:04 +0300

    committ

---
+++ src/NullPointerException/NullPointerException.java
@@ -0,0 +1,9 @@
+package NullPointerException;
+
+public class NullPointerException {
+    public static void main(String[] args) {
+        String salah=null;
+        System.out.println(salah);
+    }
+
+}
