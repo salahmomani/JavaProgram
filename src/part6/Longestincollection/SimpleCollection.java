eb96bb7d38d826fb513f3c1efe96759028805f7a
salahmomani <slhalmomani@gmail.com>
2025-02-16 15:14:39 +0300

    committ

---
+++ src/part6/Longestincollection/SimpleCollection.java
@@ -0,0 +1,52 @@
+package part6.Longestincollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleCollection {
+    private String name;
+    private List<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        elements.add(element);
+    }
+    public String longest(){
+        String test="";
+        for (String s:elements)
+        {
+            if(s.length()>test.length())
+            {
+                test=s;
+            }
+
+        }
+        if(elements.isEmpty())
+        {return "null";}
+        return  test;
+
+    }
+    @Override
+    public String toString() {
+        if (elements.isEmpty()) {
+            return "The collection " + name + " is empty.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("The collection ").append(name).append(" has ").append(elements.size()).append(" element");
+        if (elements.size() > 1) {
+            sb.append("s");
+        }
+        sb.append(":\n");
+
+        for (String element : elements) {
+            sb.append(element).append("\n");
+        }
+
+        return sb.toString().trim();
+    }
+}
---
+++ src/part6/Longestincollection/mainSC.java
@@ -0,0 +1,14 @@
+package part6.Longestincollection;
+
+public class mainSC {
+    public static void main(String[] args) {
+        SimpleCollection j = new SimpleCollection("characters");
+        System.out.println("Longest: " + j.longest());
+
+        j.add("magneto");
+        j.add("mystique");
+        j.add("phoenix");
+
+        System.out.println("Longest: " + j.longest());
+    }
+}
