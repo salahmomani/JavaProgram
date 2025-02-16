b0c52a3c6726defb751fcee50787f80961ccb757
salahmomani <slhalmomani@gmail.com>
2025-02-16 17:10:35 +0300

    committ

---
+++ src/part6/Cargohold/Item.java
@@ -0,0 +1,30 @@
+package part6.Cargohold;
+
+public class Item {
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + weight + " kg)";
+    }
+
+    public static void main(String[] args) {
+        Item book = new Item("Book", 2);
+        System.out.println(book);
+      }
+}
+
---
+++ src/part6/Cargohold/Suitcase.java
@@ -0,0 +1,38 @@
+package part6.Cargohold;
+
+import java.util.ArrayList;
+
+public class Suitcase {
+int currentweight;
+    int maxweight;
+ArrayList<Item> items;
+public  Suitcase(int maxweight ){
+this.currentweight=0;
+this.maxweight=maxweight;
+items=new ArrayList<>();
+
+}
+    public void addItem(Item item){
+    items.add(item);
+    currentweight+=item.getWeight();
+    }
+
+    @Override
+    public String toString() {
+    int size;
+    size=items.size();
+if(size==0)
+{
+
+    return "no item"+currentweight;
+}
+else {
+    if (size==1)
+    {
+        return size+" item"+currentweight;
+    }
+    else
+        return size+" items"+currentweight;
+}
+    }
+}
