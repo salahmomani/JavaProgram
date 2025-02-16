b97169fdeb6ddbb991c0426b2e06580e52e745f0
salahmomani <slhalmomani@gmail.com>
2025-02-16 16:35:02 +0300

    committ

--- src/part6/Heightorer/Room.java
+++ src/part6/Heightorer/Room.java
@@ -17,6 +17,21 @@ public class Room {
     public ArrayList<Person> getPersons(){
         return this.persons;
     }
+    public Person shortest(){
+        if(persons.isEmpty()){return null;}
+        Person shortest=persons.get(0);
+        for (Person p:persons)
+        {
+            if(p.getHeight()<shortest.getHeight()){
+                shortest=p;
 
-
+            }
+        }
+        return shortest;
+    }
+    public Person take(){
+        Person per=shortest();
+        persons.remove(per );
+        return per;
+    }
 }
