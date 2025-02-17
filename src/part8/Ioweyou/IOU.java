e1e528ec1086e7af39f520c9e4e193570ce052d4
salahmomani <slhalmomani@gmail.com>
2025-02-17 14:45:34 +0300

    committ

---
+++ src/part8/Ioweyou/IOU.java
@@ -0,0 +1,25 @@
+package part8.Ioweyou;
+
+import java.util.HashMap;
+
+public class IOU {
+
+HashMap<String,Double> hashList=new HashMap<>();
+    public IOU(){
+        this.hashList=new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount){
+hashList.put(toWhom,amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom){
+        for (String s:hashList.keySet())
+        {
+            if(s.equals(toWhom)){
+                return hashList.get(s);
+            }
+        }
+        return 0.0;
+    }
+}
