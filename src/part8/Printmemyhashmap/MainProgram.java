70eee77b43d01d38935921dd25079111482ff2b1
salahmomani <slhalmomani@gmail.com>
2025-02-17 14:15:53 +0300

    committ

---
+++ src/part8/Printmemyhashmap/MainProgram.java
@@ -0,0 +1,21 @@
+package part8.Printmemyhashmap;
+
+
+import java.util.HashMap;
+
+import static part8.Printmemyhashmap.Program.*;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+    }
+}
---
+++ src/part8/Printmemyhashmap/Program.java
@@ -0,0 +1,32 @@
+package part8.Printmemyhashmap;
+
+import java.util.HashMap;
+
+public class Program {
+    HashMap<String,String>listHash=new HashMap<>();
+
+    public static void printKeys(HashMap<String,String> hashmap)
+    {
+        for (String s:hashmap.keySet())
+        {
+            System.out.println(s);
+        }
+    }
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text){
+
+        for (String s:hashmap.keySet())
+        {
+            if(s.contains(text)){
+            System.out.println(s);
+        }
+        }
+    }
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text){
+        for (String s:hashmap.keySet())
+        {
+            if(s.contains(text)){
+                System.out.println(hashmap.get(s));
+            }
+        }
+    }
+}
