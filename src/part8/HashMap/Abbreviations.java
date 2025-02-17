4baa7c7ac13ae54412bd766316aa18c9b95e9f84
salahmomani <slhalmomani@gmail.com>
2025-02-17 13:52:29 +0300

    committ

---
+++ src/part8/HashMap/Abbreviations.java
@@ -0,0 +1,36 @@
+package part8.HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Abbreviations {
+
+    String text = "";
+           HashMap <String ,String> list=new HashMap<>();
+//      ArrayList <String> explanation=new ArrayList<>();
+//      ArrayList <String> abbreviation=new ArrayList<>();
+
+    public void addAbbreviation(String abbreviation, String explanation) {
+          list.put(abbreviation,explanation);
+
+    }
+
+    public boolean hasAbbreviation(String abbreviation) {
+            if(this.list.containsKey(abbreviation));
+            {
+                return true;
+            }
+
+    }
+
+    public String findExplanationFor(String abbreviation){
+
+            if(this.list.containsKey(abbreviation))
+            {
+                return  this.list.get(abbreviation);
+            }
+
+
+        return null;
+    }
+}
