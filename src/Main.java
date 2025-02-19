8cb6cb13a02d59f2afa7ea26d3e2dbd804bb1073
salahmomani <slhalmomani@gmail.com>
2025-02-20 01:16:16 +0300

    committ

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+            Student student = new Student("Kenny", 23, 140);
+
+            Person person = student;
+            Object object = student;
+
+            System.out.print(student + ", ");
+            System.out.print(person + ", ");
+            System.out.println(object);
+    }
+}
