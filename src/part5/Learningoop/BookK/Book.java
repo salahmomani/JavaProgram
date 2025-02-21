4c8e5ee360448cf49f3d3569fb99f8c399b079b8
salahmomani <slhalmomani@gmail.com>
2025-02-22 00:55:49 +0300

    committ

---
+++ src/part5/Learningoop/BookK/Book.java
@@ -0,0 +1,32 @@
+package part5.Learningoop.BookK;
+
+public class Book {
+    String author;
+    String Title;
+    int pagecount;
+    public Book(String author, String name, int pages){
+        this.author=author;
+        this.pagecount=pages;
+        this.Title=name;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPagecount() {
+        return pagecount;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "author " + author + '\'' +
+                ", Title: " + Title +
+                ", pagecount=" + pagecount ;
+    }
+}
---
+++ src/part5/Learningoop/BookK/mainBook.java
@@ -0,0 +1,8 @@
+package part5.Learningoop.BookK;
+
+public class mainBook {
+    public static void main(String[] args) {
+        Book book=new Book("J. K. Rowling","Harry Potter",223);
+        System.out.println(  book.toString());
+    }
+}
